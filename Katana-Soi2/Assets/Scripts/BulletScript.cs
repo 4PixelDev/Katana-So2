8649d7f653e54efa04e8d57ac1d83544ecf0893c
PixelNass <nasserdevart@gmail.com>
2023-09-07 15:34:18 +0400

    d

--- Katana-Soi2/Assets/Scripts/BulletScript.cs
+++ Katana-Soi2/Assets/Scripts/BulletScript.cs
@@ -7,19 +7,42 @@ public class BulletScript : MonoBehaviour
     public float bulletSpeed;
     private Rigidbody2D rb;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Transform ballCenter;
+    [SerializeField] private float ballRadius;
+
+    private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = Vector2.left * bulletSpeed;
     }
 
-     private void OnCollisionEnter2D(Collision2D collision)
-     {
-         if (collision.gameObject.tag == "Player")
-         {
-             Destroy(gameObject);
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Destroy(gameObject);
+        }
+    }
 
-         }
-     }
+    private void Update()
+    {
+        // Check for collisions with AttackRange
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(ballCenter.position, ballRadius);
+
+        foreach (Collider2D collider in colliders)
+        {
+            if (collider.CompareTag("Player"))
+            {
+                // Do something when the ballRadius collides with AttackRange
+                // For example, you can call a method or execute custom logic
+                collider.GetComponent<PlayerCombat>();
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(ballCenter.position, ballRadius);
+    }
 }
